a4a84efde3cac52662294f7f0eea6aefac19b37a
Matteo Gavagnin <m@macteo.it>
2017-03-30 13:57:02 +0200

    Refactored and simplified the Chooser controller.

--- Catalog/UIKitten Catalog/ChooserViewController.swift
+++ Catalog/UIKitten Catalog/ChooserViewController.swift
@@ -13,58 +13,57 @@ class ChooserViewController: TableController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let overview = Item(title: "Overview") { (cell, selected) in
-            guard selected else { return }
-            let controller = OverviewViewController()
-            self.show(controller, sender: nil)
-        }
-        
-        let charts = Item(title: "Charts") { (cell, selected) in
-            guard selected else { return }
-            let controller = ChartsViewController()
-            self.show(controller, sender: nil)
-        }
-        
-        let buttons = Item(title: "Buttons") { (cell, selected) in
-            guard selected else { return }
-            let controller = ButtonsController()
-            self.show(controller, sender: nil)
-        }
-        
-        let textFields = Item(title: "Text Fields") { (cell, selected) in
-            guard selected else { return }
-            let controller = TextFieldsController()
-            self.show(controller, sender: nil)
-        }
-        
-        let layout = Item(title: "Layout") { (cell, selected) in
-            guard selected else { return }
-            if let controller = self.storyboard?.instantiateViewController(withIdentifier: "layoutViewController") {
-                self.show(controller, sender: nil)
-            }
-        }
-        
-        let grouped = Item(title: "Grouped Buttons") { (cell, selected) in
-            guard selected else { return }
-            let controller = GroupedViewController()
-            self.show(controller, sender: nil)
-        }
-        
-        let list = Item(title: "List") { (cell, selected) in
-            guard selected else { return }
-            let controller = ListController()
-            self.show(controller, sender: nil)
-        }
-        
-        let cells = Item(title: "Cells") { (cell, selected) in
-            guard selected else { return }
-            let controller = CellsViewController()
-            self.show(controller, sender: nil)
-        }
+        let overview = forgeCell(title: "Overview")
+        let charts = forgeCell(title: "Charts")
+        let buttons = forgeCell(title: "Buttons")
+        let textFields = forgeCell(title: "Text Fields")
+        let layout = forgeCell(title: "Layout")
+        let grouped = forgeCell(title: "Grouped Buttons")
+        let list = forgeCell(title: "List")
+        let cells = forgeCell(title: "Cells")
         
         items = [
             [overview, charts, buttons, textFields, layout, grouped, list, cells]
         ]
+    }
+    
+    func forgeCell(title: String) -> Item {
+        let cell = Item(title: title) { (cell, selected) in
+            guard selected else { return }
+            var controller : UIViewController?
+            switch title {
+            case "Overview":
+                controller = OverviewViewController()
+                break
+            case "Charts":
+                controller = ChartsViewController()
+                break
+            case "Buttons":
+                controller = ButtonsController()
+                break
+            case "Text Fields":
+                controller = TextFieldsController()
+                break
+            case "Layout":
+                controller = self.storyboard?.instantiateViewController(withIdentifier: "layoutViewController")
+                break
+            case "Grouped Buttons":
+                controller = GroupedViewController()
+                break
+            case "List":
+                controller = ListController()
+                break
+            case "Cells":
+                controller = CellsViewController()
+                break
+            default:
+                break
+            }
+            guard let c = controller else { return }
+            c.navigationItem.title = title
+            self.show(c, sender: nil)
+        }
+        return cell
     }
 
 }
